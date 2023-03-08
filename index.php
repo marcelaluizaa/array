548216b23467a26de70a3c93067a940107857c01
MARCELA LUIZA RAMOS REIS <marcela.6966@aluno.pr.senac.br>
2023-03-08 11:55:05 -0300

    salvar

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php 
+ 
+ $pessoa1 = array () ;
+ $pessoa2 = array () ;
+ $pessoa3 = array () ;
+ $pessoa4 = array () ;
+ 
+ $pessoa1[0][0][0][1][1][0] = "Maria";
+ $pessoa1[0][0][0][1][1][1] = "16 ";
+ $pessoa1[0][0][0][1][1][2] = "Ivaiporã";
+
+ 
+
+ $pessoa2[0][0][0][1][1][0] = "José";
+ $pessoa2[0][0][0][1][1][1] = "11 anos";
+ $pessoa2[0][0][0][1][1][2] = "Curitiba";
+
+ 
+ $pessoa3[0][0][0][1][1][0] = "Matheus";
+ $pessoa3[0][0][0][1][1][1] = "15 anos";
+ $pessoa3[0][0][0][1][1][2] = "Curitiba"; 
+
+ 
+ 
+ $pessoa4[0][0][0][1][1][0] = "Letícia";
+ $pessoa4[0][0][0][1][1][1] = "15 anos";
+ $pessoa4[0][0][0][1][1][2] = "Ivaiporã"; 
+
+ echo  $pessoa1[0][0][0][1][1][0] . " tem " .  $pessoa1[0][0][0][1][1][1] . "anos";
+
+?>
